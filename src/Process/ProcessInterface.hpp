0f2d24e8a602b2baebd228634f2ad4f23ed7ac3e
Roland Rusch <roland@rusch.lu>
2024-09-06 16:09:27 +0200

    #33 Implement simple ProcessInterface

--- src/Process/ProcessInterface.hpp
+++ src/Process/ProcessInterface.hpp
@@ -7,24 +7,42 @@
 #define LIBSMART_STM32COMMON_PROCESS_PROCESSINTERFACE_HPP
 
 namespace Stm32Common::Process {
+    /**
+     * @brief Abstract interface for handling processing logic in the system.
+     *
+     * The `ProcessInterface` class defines a set of pure virtual functions that must be implemented by any derived class.
+     * These functions are designed to manage the lifecycle of a process, including setup, operation, and teardown.
+     */
     class ProcessInterface {
     public:
         virtual ~ProcessInterface() = default;
 
         /**
-       * @brief This pure virtual function is part of the MachineComponentInterface and needs to be overridden by derived classes.
-       *
-       * This function is responsible for setting up the machine component. It should be used to initialize any necessary resources, configurations, or variables.
-       * The derived classes should implement their own setup logic based on their specific requirements.
-       */
+         * @brief Pure virtual function to set up the initial state of the process.
+         *
+         * This method must be implemented by any class that inherits from ProcessInterface.
+         * It should be used to initialize all necessary components, allocate resources,
+         * or perform any other setup operations required before the process can start running.
+         */
         virtual void setup() = 0;
 
         /**
-         * @brief This pure virtual function is part of the MachineComponentInterface and needs to be overridden by derived classes.
+         * @brief Pure virtual function to run the main processing loop.
          *
-         * This function is called repeatedly in the main program loop and is responsible for performing the main tasks or computations of the machine component.
+         * This method must be implemented by any class that inherits from ProcessInterface.
+         * It should encompass the primary logic that needs to be repeatedly executed
+         * during the lifecycle of the process.
          */
         virtual void loop() = 0;
+
+        /**
+         * @brief Pure virtual function to terminate the process.
+         *
+         * This method must be implemented by any class that inherits from ProcessInterface.
+         * It should be used to clean up all necessary components, deallocate resources,
+         * or perform any other teardown operations required after the process has completed.
+         */
+        virtual void end() = 0;
     };
 }
 #endif
